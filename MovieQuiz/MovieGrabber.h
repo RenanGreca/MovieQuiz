cc27023b2c0d29fd423af9e5f13ed0ef95e2fb20
Renan Greca <renangreca@Game.local>
2015-03-18 20:06:59 -0300

    initial commit

---
+++ MovieQuiz/MovieGrabber.h
@@ -0,0 +1,13 @@
+//
+//  MovieGrabber.h
+//  MovieQuiz
+//
+//  Created by Renan Greca on 3/18/15.
+//  Copyright (c) 2015 Renan Greca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovieGrabber : NSObject
+
+@end
---
+++ MovieQuiz/MovieGrabber.m
@@ -0,0 +1,38 @@
+//
+//  MovieGrabber.m
+//  MovieQuiz
+//
+//  Created by Renan Greca on 3/18/15.
+//  Copyright (c) 2015 Renan Greca. All rights reserved.
+//
+
+#import "MovieGrabber.h"
+
+@interface MovieGrabber () {
+    NSString *apiKey;
+    NSString *apiUrl;
+}
+@end
+
+@implementation MovieGrabber
+
+- (NSString *) getDataFrom:(NSString *)url{
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setHTTPMethod:@"GET"];
+    [request setURL:[NSURL URLWithString:url]];
+    
+    NSError *error = [[NSError alloc] init];
+    NSHTTPURLResponse *responseCode = nil;
+    
+    NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+    
+    if([responseCode statusCode] != 200){
+        NSLog(@"Error getting %@, HTTP status code %li", url, (long)[responseCode statusCode]);
+        return nil;
+    }
+    
+    return [[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding];
+}
+
+
+@end
